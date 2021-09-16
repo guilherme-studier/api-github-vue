7f1a03344893f6bf5ccd671ebdd7bf2afdb4b41a
Guilherme Studier <guilherme.studier@pmweb.com.br>
2021-09-16 12:22:28 -0300

    quarto commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="nav">
     <router-view />
-      <div>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </div>
   </div>
 </template>
 
